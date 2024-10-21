e733d2755db59c4586d50b68b9446a82d08bc5c4
Yiannis Vozikis <vozikisyannis@gmail.com>
2024-10-21 16:07:30 +0300

    Add the game logic to display the winner and the scores

--- script.js
+++ script.js
@@ -14,39 +14,48 @@ function getHumanChoice() {
    return playerChoice
 }
 
-let computerScore = 0
-let humanScore = 0
+function playGame() {
+    let computerScore = 0
+    let humanScore = 0
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === "rock") {
-        if (computerChoice === "scissors") {
-            humanScore++
-            return "You win! Rock beats scissors!"
-        } else if (computerChoice === "paper") {
-            computerScore++
-            return "You lose! Paper beats rock!"
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === "rock") {
+            if (computerChoice === "scissors") {
+                humanScore++
+                return "You win! Rock beats scissors!"
+            } else if (computerChoice === "paper") {
+                computerScore++
+                return "You lose! Paper beats rock!"
+            } else {
+                return "It's a tie! You both chose rock!"
+            }
+        } else if (humanChoice === "paper") {
+            if (computerChoice === "scissors") {
+                computerScore++
+                return "You lose! Scissors beat paper!"
+            } else if (computerScore === "rock") {
+                humanScore++
+                return "You win! Paper beats rock!"
+            } else {
+                return "It's a tie! You both chose paper!"
+            }
         } else {
-            return "It's a tie! You both chose rock!"
-        }
-    } else if (humanChoice === "paper") {
-        if (computerChoice === "scissors") {
-            computerScore++
-            return "You lose! Scissors beat paper!"
-        } else if (computerScore === "rock") {
-            humanScore++
-            return "You win! Paper beats rock!"
-        } else {
-            return "It's a tie! You both chose paper!"
-        }
-    } else {
-        if (computerChoice === "rock") {
-            computerScore++
-            return "You lose! Rock beats scissors!"
-        } else if (computerChoice === "paper") {
-            humanScore++
-            return "You win! Scissors beat paper!"
-        } else {
-            return "It's a tie! You both chose scissors!"
+            if (computerChoice === "rock") {
+                computerScore++
+                return "You lose! Rock beats scissors!"
+            } else if (computerChoice === "paper") {
+                humanScore++
+                return "You win! Scissors beat paper!"
+            } else {
+                return "It's a tie! You both chose scissors!"
+            }
         }
     }
+
+    const player = getHumanChoice()
+    const computer = getComputerChoice()
+
+    console.log(playRound(player, computer))
+    console.log(`Player score: ${humanScore}`)
+    console.log(`Computer score: ${computerScore}`)
 }
