8591a32ad36c4120588e37c1eebe86bd5bb07beb
Yiannis Vozikis <vozikisyannis@gmail.com>
2024-10-21 15:44:53 +0300

    Fix getComputerChoice to assign the numbers to actual choices

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3 + 1)
+    let choice = Math.floor(Math.random() * 3 + 1)
+    if (choice === 1) {
+        return "rock"
+    } else if (choice === 2) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
 }
 
 function getHumanChoice() {
