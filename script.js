7b66779541ba3d8bb9d233e7352a4b409e1fbdd5
Yiannis Vozikis <vozikisyannis@gmail.com>
2024-10-21 15:55:51 +0300

    Write the logic for the game

--- script.js
+++ script.js
@@ -13,3 +13,40 @@ function getHumanChoice() {
    let playerChoice = prompt("Please enter your choice (rock, paper, or scissors): ").toLowerCase()
    return playerChoice
 }
+
+let computerScore = 0
+let humanScore = 0
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock") {
+        if (computerChoice === "scissors") {
+            humanScore++
+            return "You win! Rock beats scissors!"
+        } else if (computerChoice === "paper") {
+            computerScore++
+            return "You lose! Paper beats rock!"
+        } else {
+            return "It's a tie! You both chose rock!"
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            computerScore++
+            return "You lose! Scissors beat paper!"
+        } else if (computerScore === "rock") {
+            humanScore++
+            return "You win! Paper beats rock!"
+        } else {
+            return "It's a tie! You both chose paper!"
+        }
+    } else {
+        if (computerChoice === "rock") {
+            computerScore++
+            return "You lose! Rock beats scissors!"
+        } else if (computerChoice === "paper") {
+            humanScore++
+            return "You win! Scissors beat paper!"
+        } else {
+            return "It's a tie! You both chose scissors!"
+        }
+    }
+}
