307ad39e6f65ce2ba2b9825ce0b7f369c4e9ee64
Yiannis Vozikis <vozikisyannis@gmail.com>
2024-10-21 15:41:36 +0300

    Write the function to get the player's choice

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function getComputerChoice() {
     return Math.floor(Math.random() * 3 + 1)
 }
+
+function getHumanChoice() {
+   let playerChoice = prompt("Please enter your choice (rock, paper, or scissors): ").toLowerCase()
+   return playerChoice
+}
